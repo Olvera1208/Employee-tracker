92e6349cce4221dc09c37b47152d994351c6c025
isiah Olvera <itolvera98@gmail.com>
2022-02-02 23:10:33 -0800

    updating my schema.sql

--- schema.sql
+++ schema.sql
@@ -1,29 +1,31 @@
-DROP DATABASE IF EXISTS employee_DB;
+DROP DATABASE IF EXISTS employees_db;
 
-CREATE DATABASE employee_DB;
+CREATE DATABASE employees_db;
 
-USE employee_DB;
+USE employees_db;
 
--- DEPARTMENT TABLE ----
+-- table for department
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30)
- 
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
--- DEPARTMENT TABLE ----
+
+-- table for roles
 CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
-  department_id INT,
-  FOREIGN KEY (department_id) REFERENCES department(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
--- EMPLOYEE ROLE TABLE ----
+
+-- table for employees
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  manager_id INT,
-  role_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
