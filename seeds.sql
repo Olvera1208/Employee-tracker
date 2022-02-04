5228e70117452546fa1b33bd0e7ed4534990949b
isiah Olvera <itolvera98@gmail.com>
2022-02-03 22:54:45 -0800

    deleted app.js

--- seeds.sql
+++ seeds.sql
@@ -10,18 +10,18 @@ VALUES
 
 INSERT INTO role (title, salary, department_id)
 VALUES
-('Legal Team Lead', 90000, 1),
-('Software Engineer', 70000, 2),
-('Web Developer', 50000, 3),
-('Manager', 70000, 4),
-('Lead Engineer', 60000, 5),
+('Legal Team Lead', 78000, 1),
+('Software Engineer', 67000, 2),
+('Web Developer', 30000, 3),
+('Manager', 15000, 4),
+('Lead Engineer', 20000, 5),
 ('Sales Rep', 40000, 6);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-('Isiah', 'Olvera', 1, 458),
-('Peter', 'Parker', 2, 276),
-('Tony', 'Stark', 3, 486),
-('Steve', 'Rogers', 4, 126),
-('Peter', 'Quill', 5, 724),
-('Clint', 'Barton', 6, 157);
+('Isiah', 'Olvera', 1, 980),
+('Peter', 'Parker', 2, 760),
+('Tony', 'Stark', 3, 445),
+('Steve', 'Rogers', 4, 157),
+('Peter', 'Quill', 5, 853),
+('Clint', 'Barton', 6, 657);
